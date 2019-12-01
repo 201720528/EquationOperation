0973276efacc31434f6f9b6fc19978626fbfc295
201720528 <57162628+201720528@users.noreply.github.com>
2019-12-01 14:18:22 +0800

    Update and rename GenerateEquations.java to AdditionOperation.java

---
+++ AdditionOperation.java
@@ -0,0 +1,25 @@
+package test5;
+
+public class  extends Equation{
+
+	//通过构造方法调用父类generateBinaryOperation方法
+	public AdditionOperation() {
+		// TODO Auto-generated constructor stub
+		generateBinaryOperation('+');
+	}
+	
+	//计算结果
+	@Override
+	protected int calculate(int left, int right) {
+		// TODO Auto-generated method stub
+		return left + right;
+	}
+	
+	
+	@Override
+	protected boolean checkCalculation(int result) {
+		// TODO Auto-generated method stub
+		return result<=100;
+	}
+
+}

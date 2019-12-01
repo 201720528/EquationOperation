4a7dfcf38a1586ee7c26ac43bbc62363468b195b
201720528 <57162628+201720528@users.noreply.github.com>
2019-12-01 14:21:41 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,20 @@
+package test5;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		//50道加法
+		Exercise exercise1 = new Exercise();
+		exercise1.generateAdditionExercise(50);
+		exercise1.formatDisplay();
+		//50道减法
+		Exercise exercise2 = new Exercise();
+		exercise2.generateSubstractExercise(50);
+		exercise2.formatDisplay();
+		//50道混合
+		Exercise exercise3 = new Exercise();
+		exercise3.generateBinaryExercise(50);
+		exercise3.formatDisplay();
+	}
+}
